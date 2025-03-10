1dd6d683c81aff2b8903fde78f96f7af29c92156
ives-yuji <ivesyuji@gmail.com>
2025-03-10 19:13:59 -0300

    form-validation

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -10,8 +10,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class DefaultLoginLayoutComponent {
   @Input() primaryBtnText: string = "";
   @Input() secondaryBtnText: string = "";
+  @Input() disablePrimaryBtn: boolean = true;
   @Output("submit") onSubmit = new EventEmitter();
   @Output("navigate") onNavigate = new EventEmitter();
+  
 
   submit(){
     this.onSubmit.emit();
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 interface LoginForm {
   email: FormControl,
@@ -15,13 +18,20 @@ interface LoginForm {
     ReactiveFormsModule,
     PrimaryInputComponent
   ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup<LoginForm>;
 
-  constructor(){
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+  ){
     this.loginForm = new FormGroup({
 
         email: new FormControl('',[Validators.required, Validators.email]),
@@ -31,11 +41,14 @@ export class LoginComponent {
   }
 
   submit(){
-    console.log(this.loginForm.value)
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success("Login feito com Sucesso ! "),
+      error: () => this.toastService.success("Erro ! ")
+    })
   }
 
   navigate(){
-    console.log(this.loginForm.value)
+    this.router.navigate(["signup"])
   }
 
 
