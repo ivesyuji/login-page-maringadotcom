233758c561bf86b95f09a645c3741ac1c34e469d
ives-yuji <ivesyuji@gmail.com>
2025-03-10 16:57:45 -0300

    teste com login

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm!: FormGroup<LoginForm>;
+
+  constructor(){
+    this.loginForm = new FormGroup({
+
+        email: new FormControl('',[Validators.required, Validators.email]),
+        password: new FormControl('',[Validators.required, Validators.minLength(6)])
+
+      })
+  }
+
+  submit(){
+    console.log(this.loginForm.value)
+  }
+
+  navigate(){
+    console.log(this.loginForm.value)
+  }
+
+
+}
+
